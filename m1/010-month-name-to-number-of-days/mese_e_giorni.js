74460e6c5bdec32a0e9160da91b9175d836a1eda
GiuseppePad17 <giuseppe17padula@gmail.com>
2023-01-25 17:19:16 +0100

    aggiunta esercizio 010 - M1

---
+++ m1/010-month-name-to-number-of-days/mese_e_giorni.js
@@ -0,0 +1,52 @@
+var mese = prompt("inserisci il mese");
+
+switch (mese) {
+        case "gennaio":
+            window.alert("31 giorni");
+        break;
+
+        case "febbraio":
+            window.alert("28 o 29 giorni");
+        break;
+        
+
+        case "marzo":
+            window.alert("31 giorni");
+        break;
+
+        case "aprile":
+            window.alert("30 giorni");
+        break;
+
+        case "maggio":
+            window.alert("31 giorni");
+        break;
+
+        case "giugno":
+            window.alert("30 giorni");
+        break;
+
+        case "luglio":
+            window.alert("31 giorni");
+        break;
+
+        case "agosto":
+            window.alert("31 giorni");
+        break;
+
+        case "settembre":
+            window.alert("30 giorni");
+        break;
+
+        case "ottobre":
+            window.alert("31 giorni");
+        break;
+
+        case "novembre":
+            window.alert("30 giorni");
+        break;
+
+        case "dicembre":
+            window.alert("31 giorni");
+        break;
+}
